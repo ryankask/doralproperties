35d9c2a6ef65c5f4db1c90865ebd98590c24402c
Ryan Kaskel <dev@ryankaskel.com>
2013-05-11 14:07:20 +0100

    Intial commit.

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source :rubygems
+
+gem "compass", "0.12.2"
+gem "modular-scale", "1.0.6"
+gem "susy", "1.0.8"
+gem "sassy-buttons", "0.1.4"
---
+++ config.rb
@@ -0,0 +1,10 @@
+require 'modular-scale'
+require 'susy'
+
+http_path = "/"
+sass_dir = "sass"
+css_dir = "app/static/css"
+images_dir = "app/static/images"
+javascripts_dir = "app/static/js"
+
+output_style = :compact #:compressed
