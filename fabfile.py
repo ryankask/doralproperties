e0b9b6c40694c450c391c9c3d8eb882356056501
Ryan Kaskel <dev@ryankaskel.com>
2013-05-14 13:16:44 +0100

    Use fabric for deployment.

---
+++ fabfile.py
@@ -0,0 +1,12 @@
+from fabric.api import env, require, cd, run
+
+env.hosts = ['doralproperties.us']
+env.user = 'deploy'
+env.deploy_path = '~/apps/doralproperties'
+
+
+def deploy():
+    require('deploy_path')
+
+    with cd(env.deploy_path):
+        run('git pull')
