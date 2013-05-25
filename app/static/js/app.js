1a3956c4bb253714fbfe5cc6738518225d13ebb2
Ryan Kaskel <dev@ryankaskel.com>
2013-05-25 17:59:23 +0100

    Change background color of nav link for current page.

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,19 +1,22 @@
-'use strict';
+(function() {
+  'use strict';
 
-var app = angular.module('doralProperties', []);
+  var deps = ['doralProperties.controllers', 'doralProperties.directives'],
+      app = angular.module('doralProperties', deps);
 
-app.config(function($routeProvider, $locationProvider) {
-  $routeProvider.when('/page/about', {
-    templateUrl: '/partials/about.html'
-  }).when('/page/properties', {
-    templateUrl: '/partials/properties.html'
-  }).when('/page/services', {
-    templateUrl: '/partials/services.html'
-  }).when('/page/social', {
-    templateUrl: '/partials/social.html'
-  }).otherwise({
-    templateUrl: '/partials/home.html'
+  app.config(function($routeProvider, $locationProvider) {
+    $routeProvider.when('/page/about', {
+      templateUrl: '/partials/about.html'
+    }).when('/page/properties', {
+      templateUrl: '/partials/properties.html'
+    }).when('/page/services', {
+      templateUrl: '/partials/services.html'
+    }).when('/page/social', {
+      templateUrl: '/partials/social.html'
+    }).otherwise({
+      templateUrl: '/partials/home.html'
+    });
+
+    $locationProvider.html5Mode(true);
   });
-
-  $locationProvider.html5Mode(true);
-});
+})();
---
+++ app/static/js/controllers.js
@@ -0,0 +1,6 @@
+(function() {
+  'use strict';
+
+  var controllers = angular.module('doralProperties.controllers', []);
+  controllers.controller('AppCtrl', function($scope) {});
+})();
