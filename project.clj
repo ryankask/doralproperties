fb607836f1e7f6974006f034cd7a3eb73c4f40bc
Ryan Kaskel <dev@ryankaskel.com>
2014-06-04 12:35:30 +0100

    Update Clojure dependencies.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject doralprops "0.1.0-SNAPSHOT"
+(defproject doralprops "0.2.0-SNAPSHOT"
   :description "Doral Properties crawler template rendering."
   :url "https://github.com/ryankask/doralproperties"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.1.8"]
-                 [http-kit "2.1.4"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring-core "1.3.0"]
+                 [http-kit "2.1.18"]
                  [me.raynes/laser "1.1.1"]]
   :plugins [[lein-ring "0.8.5"]]
-  :profiles {:dev {:dependencies [[ring/ring-jetty-adapter  "1.1.0"]
+  :profiles {:dev {:dependencies [[ring/ring-jetty-adapter  "1.3.0"]
                                   [ring-mock "0.1.5"]]}}
   :resource-paths ["app"]
   :ring {:handler doralprops.core/app}
